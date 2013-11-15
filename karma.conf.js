006ac00150c7bf4484aaafa15b509ee5ced34fd8
Vikas Goyal <vikasgoyalgzs@gmail.com>
2013-11-15 15:33:50 -0500

    Fixed issues with bower install on windows

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
             'app/bower_components/angular-sanitize/angular-sanitize.js',
             'app/bower_components/angular-route/angular-route.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
-            'app/bower_components/ui-bootstrap/ui-bootstrap-0.6.0.js',
+            'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
             'app/scripts/*.js',
             'app/scripts/**/*.js',
             'test/spec/**/*.js'
