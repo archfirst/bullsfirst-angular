a57d868aca2080667e906966e18dfcdb071fb882
Solh Zendeh <szendeh@yahoo.com>
2013-12-06 15:54:33 -0500

    added e2e testing - just a single test at this time - tweaked the login test just to get rid of the error - tweaked e2e conf to use my dev server - found an error in the app during testing and fixed (multiple tbodies)!

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -24,10 +24,8 @@ module.exports = function (config) {
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
-
 
         // Start these browsers, currently available:
         // - Chrome
@@ -39,16 +37,19 @@ module.exports = function (config) {
         // - IE (only Windows)
         browsers: ['Chrome'],
 
-
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
-        singleRun: false
+        singleRun: false,
 
         // Uncomment the following lines if you are using grunt's server to run the tests
-        // proxies: {
-        //   '/': 'http://localhost:9000/'
-        // },
+        proxies: {
+            // NOTE I'm not sure if there's another way to do this. I couldn't figure one out...
+            '/': 'http://bullsfirst/app/',
+            '/bfoms-javaee/rest/': 'http://archfirst.org/bfoms-javaee/rest/',
+            '/bfexch-javaee/rest/': 'http://archfirst.org/bfexch-javaee/rest/'
+        },
+
         // URL root prevent conflicts with the site root
-        // urlRoot: '_karma_'
+        urlRoot: '_karma_'
     });
 };
